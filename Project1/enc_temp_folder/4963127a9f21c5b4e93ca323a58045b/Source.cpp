2dc402c37d145709fd99d252f5c37707c24e9702
Keny Ramos <keny.ramosp@gmail.com>
2017-08-22 14:44:49 -0500

    project1

--- Project1/enc_temp_folder/4963127a9f21c5b4e93ca323a58045b/Source.cpp
+++ Project1/enc_temp_folder/4963127a9f21c5b4e93ca323a58045b/Source.cpp
@@ -7,18 +7,15 @@ using namespace System;
 
 int main()
 {
-	int *monto_inicial,*cant_5,*cant_2,*cant_1;
+	int *monto_inicial,*cant_5;
 	monto_inicial = new int;
 	cant_5 = new int;
-	cant_2 = new int;
-	cant_1 = new int;
 
 	cout<<"Ingrese el monto inicial: ";
 	cin >> *monto_inicial;
 
 	*cant_5 = *monto_inicial / 5;
-	*cant_2 = (*monto_inicial % 5) / 2;
-	*cant_1 = ((*monto_inicial % 5) % 2) / 1;
+
 	cout << "cantidad de monedas de 5 soles: "<<*cant_5;
 	
 	cout << "\n";
